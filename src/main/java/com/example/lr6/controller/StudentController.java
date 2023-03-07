a4b8e77a779e431e734a325e4809f60f159c78f8
Nikaterina <xbxekaterina@yandex.ru>
2023-03-07 09:59:14 +0500

    1

--- src/main/java/com/example/lr6/controller/StudentController.java
+++ src/main/java/com/example/lr6/controller/StudentController.java
@@ -1,2 +1,63 @@
-package com.example.lr6.controller;public class StudentController {
+package com.example.lr6.controller;
+
+import com.example.lr6.entity.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import com.example.lr6.dao.StudentRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+@Slf4j
+@Controller
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping({"/list", "/"})
+    public ModelAndView getAllStudents() {
+        log.info("/list -> connection");
+        ModelAndView mav = new ModelAndView("list-students");
+        mav.addObject("students", studentRepository.findAll());
+        return mav;
+    }
+
+    @GetMapping("/addStudentForm")
+    public ModelAndView addStudentForm() {
+        ModelAndView mav = new ModelAndView("add-student-form");
+        Student student = new Student();
+        mav.addObject("student", student);
+        return mav;
+    }
+
+    @PostMapping("/saveStudent")
+    public String saveStudent(@ModelAttribute Student student) {
+        studentRepository.save(student);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/showUpdateFrome")
+    public ModelAndView showUpdateFrome(@RequestParam Long studentId) {
+        ModelAndView mav = new ModelAndView("add-student-form");
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        Student student = new Student();
+        if (optionalStudent.isPresent()){
+            student = optionalStudent.get();
+        }
+        mav.addObject("student", student);
+        return mav;
+    }
+
+    @GetMapping("deleteStudent")
+    public String deleteStudent(@RequestParam Long studentId) {
+        studentRepository.deleteById(studentId);
+        return "redirect:/list";
+    }
+
 }
